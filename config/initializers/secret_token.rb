2b216d5db4effdfa2c7ce2764e5f8983840ec861
Isac Coelho <isac.coelho@gmail.com>
2012-02-12 12:20:08 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dialetika::Application.config.secret_token = '050a438c21f4c19b798334be85e89fc9bd9aed729150ee6c701fccc2d8e46cfb7707eee46edbd2cdcfeba1f2e51f7f05b6c87b7fca71f0ad7718efe2a446bee7'
